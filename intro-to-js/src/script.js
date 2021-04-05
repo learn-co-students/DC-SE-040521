f2b2a8aaffd65db1f99b3545d762647b8ce54d1e
Adam Johnson <adamwjo@gmail.com>
2021-04-05 17:40:31 -0400

    finished lecture

---
+++ intro-to-js/src/script.js
@@ -0,0 +1,39 @@
+
+
+
+// Data-Types
+
+// Primitive Data
+
+    //Strings
+let string = "Hey I'm a string"
+
+let string2 = "Hello Everyone"
+
+let newString = `${string} ${string2} some new stuff`
+
+console.log(newString)
+
+    //Boolean
+// let adam = true
+//     console.log(!!adam)
+
+    //Number 
+let num = null
+let numString = "zero"
+
+console.log(num)
+
+    //Undefined
+    //Null 
+
+// Non-primitive Data
+
+    //Array
+let array = [1, 2, 3]
+console.log(array)
+array.push(1)
+console.log(array)
+
+    //Objects
+let adam = {name: "Adam", age: 28}
