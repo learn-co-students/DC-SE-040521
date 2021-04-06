d384ad76084d247be518d791d3bb28ec40a69ed2
Adam Johnson <adamwjo@gmail.com>
2021-04-06 09:57:54 -0400

    lecture starting point

--- 02-scope-functions-callbacks/src/script.js
+++ 02-scope-functions-callbacks/src/script.js
@@ -1,4 +1,22 @@
 
 
+// Functions 
 
+// Function Declaration 
+
+// Function Expresson
+
+
+//Global Scope
+
+//Block Scope
+
+
+//Callback functions 
+
+ //.map()
+
+ //.filter()
+
+ //.setTimeout()
 
