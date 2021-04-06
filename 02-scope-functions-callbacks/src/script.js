2374ad6c950cc5b3012070186b2c72fe86833952
Adam Johnson <adamwjo@gmail.com>
2021-04-06 10:29:44 -0400

    new starting point

--- 02-scope-functions-callbacks/src/script.js
+++ 02-scope-functions-callbacks/src/script.js
@@ -1,5 +1,17 @@
-
-
+// Review 
+    // Primitive Data
+    // Boolean
+    const boolVar = 0
+    if(boolVar){
+        console.log("this is truthy");
+    }else{
+        console.log("this is falsey");
+    }
+    // Number
+    // String
+    // Non-Primitive (objects)
+    // arrays
+    // objects
 // Functions 
 
 // Function Declaration 
