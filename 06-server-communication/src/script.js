732ed730a3019a1261fd0cafc57e567b2dd0707e
Adam Johnson <adamwjo@gmail.com>
2021-04-12 10:30:37 -0400

    todays lecture

---
+++ 06-server-communication/src/script.js
@@ -0,0 +1,17 @@
+///// USER STORIES /////
+
+// When I click the "Get Mah Monsters" button 
+    // I should make a server call to get all my monsters
+    // I should see a list item for each pokemon in the data
+
+// When I click a pokemons name, 
+    // I should see it's information rendered in the details section
+
+// When I submit a new pokemons info using the form
+    // I should see a new list item for that pokemon
+    // That list item should behave like the original ones
+    // That pokemon should be persisted in the database 
+
+// When I click a pokemons "feed me" button
+    // I should see the pokemons weight incrimented by 10
+    // That information should persist in the database 
